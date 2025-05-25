f4d1fb3cf8d79a092449dfe211f295c5bf65e85c
ChusnulBariya <165135523+ChusnulBariya@users.noreply.github.com>
2025-05-25 15:48:23 +0700

    screens

---
+++ lib/screens/sign_in_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignInScreens extends StatefulWidget {
+  const SignInScreens({super.key});
+
+  @override
+  State<SignInScreens> createState() => _SignInScreensState();
+}
+
+class _SignInScreensState extends State<SignInScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/sign_up_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreens extends StatefulWidget {
+  const SignUpScreens({super.key});
+
+  @override
+  State<SignUpScreens> createState() => _SignUpScreensState();
+}
+
+class _SignUpScreensState extends State<SignUpScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
