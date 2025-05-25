1459351b13bac86c7fec1411df5f57d80c72d406
winter <annisaallyasalsabillahh@gmail.com>
2025-05-25 15:56:29 +0700

    update depedencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MainApp extends StatelessWidget {
