6bbee5ff78af9a4f9b59dfb03f1db5673e298522
ChusnulBariya <165135523+ChusnulBariya@users.noreply.github.com>
2025-05-25 15:36:55 +0700

    push

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,9 +11,10 @@ plugins {
 android {
     namespace = "com.example.booklist"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -27,7 +28,7 @@ android {
         applicationId = "com.example.booklist"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -41,6 +42,10 @@ android {
         }
     }
 }
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
+}   
 
 flutter {
     source = "../.."
